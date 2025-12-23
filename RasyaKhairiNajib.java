89489a23c6c332cfb406fa675f2acba5303a4655
rasyakhairinajib1324091 <orangilangbintitan@gmail.com>
2025-12-23 22:44:20 +0700

    Menambahkan file perkenalan diri

---
+++ RasyaKhairiNajib.java
@@ -0,0 +1,11 @@
+
+/**
+ *
+ * @author Rasya Khairi
+ */
+public class RasyaKhairiNajib {
+
+    public static void main(String[] args) {
+        System.out.println("Saya Rasya Khairi Najib dengan NIM 1324091 dari Kelas SA03");
+    }
+}
