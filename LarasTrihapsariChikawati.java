161959a5ed82bb3556c8476b46c96feaf7fe47bb
Laras Trihapsari Chikawati <larastrichika@gmail.com>
2025-12-23 21:29:56 +0700

    Menambahkan class LarasTrihapsariChikawati
    File Java berisi nama dan nim anggota

---
+++ LarasTrihapsariChikawati.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.perkenalan;
+
+/**
+ *
+ * @author laraschika
+ */
+public class LarasTrihapsariChikawati {
+    public static void main(String[] args) {
+        System.out.println("Saya Laras Trihapsari Chikawati dengan NIM 1324071 dari Kelas SA03");
+    }
+}
+
+
